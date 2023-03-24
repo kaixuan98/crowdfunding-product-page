8c8c24a217885383d80c44cb266303b1eec9992a
KaiXuan Chin <47827034+kaixuan98@users.noreply.github.com>
2023-03-24 16:32:56 -0400

    Structured the page with semantic HTML

---
+++ crowdfunding-product-page/src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar : React.FC =  () => {
+  return (
+    <nav>
+        <ul>
+        <li>About</li>
+        <li>Discover</li>
+        <li>Get Started</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
